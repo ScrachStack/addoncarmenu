3759681a86cad8b742111b54682564d68cb956b5
KeziCodesSometimes <122732007+KeziCodesSometimes@users.noreply.github.com>
2023-05-10 15:30:32 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Kezi'
+description 'Addon Car Menu'
+version '1.0.0'
+
+client_scripts {
+      '@menuv/menuv.lua',
+    'addoncarmenu.lua'
+}
